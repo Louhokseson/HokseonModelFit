f81400114c4bd81d88cd6f2f8e809ccb41255c78
Louhokseson <974766935@qq.com>
2024-06-15 17:14:35 +0100

    first commit

---
+++ scripts/Optimization/toy.jl
@@ -0,0 +1,12 @@
+using DrWatson
+@quickactivate "HokseonModelFit"
+using Optimization
+rosenbrock(u, p) = (p[1] - u[1])^2 + p[2] * (u[2] - u[1]^2)^2
+u0 = zeros(2)
+p = [1.0, 100.0]
+
+prob = OptimizationProblem(rosenbrock, u0, p)
+
+# Import a solver package and solve the optimization problem
+using OptimizationOptimJL
+sol = solve(prob, NelderMead())
