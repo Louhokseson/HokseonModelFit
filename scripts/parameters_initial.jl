072ef20d277d2fed5fe62eb2fd3dcf8a27c9b259
Louhokseson <88427189+Louhokseson@users.noreply.github.com>
2024-06-21 18:13:20 +0100

    update

--- scripts/parameters_initial.jl
+++ scripts/parameters_initial.jl
@@ -1,6 +1,7 @@
 using DrWatson
 @quickactivate "HokseonModelFit"
 using Unitful, UnitfulAtomic
+using Serialization
 
 
 global hokseon_model_initial_params = Dict{Symbol,Float64}(
@@ -8,7 +9,7 @@ global hokseon_model_initial_params = Dict{Symbol,Float64}(
     :Dₑ => austrip(1.761u"eV"), 
     :a => austrip(1.92u"Å^-1"), 
     :x₀ => austrip(1.532u"Å"), 
-    :c => austrip(-1.706u"eV"), 
+    :c => austrip(0.0u"eV"), 
     
     ## Hokseon Logistic parameters ##
     :L => austrip(14.206u"eV"),
@@ -24,6 +25,22 @@ global hokseon_model_initial_params = Dict{Symbol,Float64}(
     :x̃ => austrip(2.7u"Å"),
 )
 
+hokseon_model_initial_params_vector = [(:x̃, 5.10226053648958), 
+                                       (:k, 1.023957903097305), 
+                                       (:x₀′, 4.643057088205517), 
+                                       (:Γ, 0.0036749322175518595), 
+                                       (:c, 0.0), 
+                                       (:c′, -0.04520166627588787), 
+                                       (:q, 0.05), 
+                                       (:a, 1.0160202449337599), 
+                                       (:ã, 0.9448630623128851), 
+                                       (:x₀, 2.89506042292668), 
+                                       (:L, 0.5220608708254171), 
+                                       (:Dₑ, 0.06471555635108824), 
+                                       (:a′, 0.975)]
+
+values_initial = [pair[2] for pair in hokseon_model_initial_params_vector]
+
 
 global general_params = Dict{String,Any}(
 
@@ -37,3 +54,16 @@ global general_params = Dict{String,Any}(
 @unpack x_ang, bandgap, centre, discretisation, nstates, width = general_params
 
 x = austrip.(x_ang .* u"Å");
+
+
+
+
+#Do it when you need to save the initial parameters
+
+
+#file_path = "parameters/initial_parameters.jls" # .jls extension for Julia Serialized file
+
+# Serialize and save the dictionary to the file
+#open(file_path, "w") do file
+#    serialize(file, hokseon_model_initial_params)
+#end
