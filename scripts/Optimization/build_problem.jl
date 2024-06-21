fc6620baa98bce90befc0901968cc496154cda2d
Louhokseson <88427189+Louhokseson@users.noreply.github.com>
2024-06-21 18:13:54 +0100

    tried some new methods

--- scripts/Optimization/build_problem.jl
+++ scripts/Optimization/build_problem.jl
@@ -1,6 +1,6 @@
 # Define the problem to solve
 using Optimization, ForwardDiff, Zygote
-using OptimizationOptimJL
+using OptimizationOptimJL, OptimizationBBO
 using Serialization
 using Polynomials
 
@@ -15,9 +15,21 @@ DFT_polynomial = open(f -> deserialize(f), polynomial_path)
 
 f = OptimizationFunction(RMSE_DFT, Optimization.AutoForwardDiff())
 
-prob = OptimizationProblem(f, hokseon_model_initial_params, DFT_polynomial)
 
-sol = solve(prob, NelderMead()) # Nelder–Mead method: derivative-free
+symbols = [:x̃, :k, :x₀′, :Γ, :c, :c′, :q, :a, :ã, :x₀, :L, :Dₑ, :a′]
+lb = [sym == :Γ ? 0.0 : -1.0 for sym in symbols]
+ub = [10.0 for sym in symbols]
+
+prob = Optimization.OptimizationProblem(f, values, DFT_polynomial, lb = lb, ub = ub)
+
+# Define a callback function to display the current iteration number
+callback = function (state)
+    println("Current iteration: ", state.iteration)
+    return false # Return false to indicate that the optimization should not terminate
+end
+
+sol = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited(), maxiters = 3,
+    maxtime = 1000.0) # Nelder–Mead method: derivative-free
 
 
 """
